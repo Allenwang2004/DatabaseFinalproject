bc94974610bcf060df6d132ea96a58b705e52065
jianhonggg <146827058+jianhonggg@users.noreply.github.com>
2024-01-09 13:48:31 +0800

    Update userdelete.inc.php

--- includes/userdelete.inc.php
+++ includes/userdelete.inc.php
@@ -1,25 +1,30 @@
 <?php
-
+require_once 'login_model.inc.php';
+require_once  "try_dbh.inc.php";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
-
+    $result = get_user($pdo,$username);
     try {
-        require_once  "try_dbh.inc.php";
 
-        $query = "DELETE FROM users WHERE username = :username AND pwd = :pwd;";
+        if(!password_verify($pwd, $result["pwd"])){
+            $_SESSION["errors_login"] = $errors;
+            header("Location: ../accounts.php");
+            die();
+        }
+
+        $query = "DELETE FROM users WHERE username = :username";
 
         $stmt = $pdo->prepare($query);
 
         $stmt->bindParam(":username",$username);
-        $stmt->bindParam(":pwd",$pwd);
 
         $stmt->execute();
 
         $pdo = null;
         $stmt = null;
 
-        header("Location: ../try_login.php");
+        header("Location: ../accounts.php");
 
         die();
     } catch (PDOException $e) {
