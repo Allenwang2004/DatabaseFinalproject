61ddc73c934ea6962472596e1f81789504fd6441
jianhonggg <146827058+jianhonggg@users.noreply.github.com>
2024-01-09 13:45:31 +0800

    Update try_login.php

--- try_login.php
+++ try_login.php
@@ -12,6 +12,7 @@ require_once 'includes/leave_comment_view.inc.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>account</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
@@ -19,61 +20,26 @@ require_once 'includes/leave_comment_view.inc.php';
         <?php output_username();?>
     </h3>
 
-    <?php if(!isset( $_SESSION["user_id"])){?>
+
+    <div class="text-center">
+
         <h3>Login</h3>
         <form action = "includes/login.inc.php" method="post">
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="pwd" placeholder="Password">
         <button>Login</button>
         </form>
-    <?php check_login_errors();} ?>
+        <?php check_login_errors(); ?>
 
-    <h3>Signup</h3>
-    <form action = "includes/signup.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <input type="text" name="email" placeholder="E-mail">
-        <button>Signup</button>
-    </form>
-    <?php
-    check_signup_errors();
-    ?>
-
-    <h3>Logout</h3>
-    <form action = "includes/logout.inc.php" method="post">
-        <button>Logout</button>
-    </form>
-
-    <h3>Change Account</h3>
-    <form action = "includes/userupdate.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <input type="text" name="email" placeholder="E-mail">
-        <button>Change</button>
-    </form>
-
-    <h3>Delete Account</h3>
-    <form action = "includes/userdelete.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <button>Delete</button>
-    </form>
-
-    <h3>Search Comment</h3>
-    <form class="searchform" action = "search.php" method="post">
-        <label for="search">Search for user's comment:</label>
-        <input id="search" type="text" name="usersearch">
-        <button>Search</button>
-    </form>
+        <h3>Signup</h3>
+        <form action = "includes/signup.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="text" name="email" placeholder="E-mail">
+            <button>Signup</button>
+        </form>
+        <?php check_signup_errors(); ?>
     
-    <?php if(isset( $_SESSION["user_id"])){?>
-    <h3>Leave a Comment</h3>
-    <form action = "includes/leave_comment.php" method="post">
-        <label for="search">Comment here:</label>
-        <input id="search" type="text" name="usercomment">
-        <button>Comment</button>
-    </form>
-    <?php check_empty_comment();} ?>
-
+    </div>
 </body>
 </html>
